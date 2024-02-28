9606c23051b604e4d6bb062724436373c68142c9
Web Ghost <108879264+HAWKZ4@users.noreply.github.com>
2024-02-28 18:26:42 +0300

    feat(orderController): Implement ( createOrder, getUserOrders, getOrdersById ) functions for managing operations on orders
    refactor(orderController): Improve function name by changing it from 'getMyOrders' to 'getUserOrders'

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,21 +5,42 @@ import Order from "../models/orderModel.js";
 // @route POST /api/orders
 // @access Private
 const createOrder = asyncHandler(async (req, res) => {
-  res.send("Create Order");
+  const order = new Order({
+    orderItems: dbOrderItems,
+    user: req.user._id,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  });
+
+  const createdOrder = await order.save();
+
+  res.status(201).json(createdOrder);
 });
 
 // @desc Get logged in user orderes
 // @route GET /api/orders/myorders
 // @access Private
-const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("Get my orders");
+const getUserOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
 });
 
 // @desc Get order by id
 // @route GET /api/orders/:id
 // @access Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("Get order by ID");
+  const order = await Order.findById(req.params.id).populate("user", "name email");
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc Update order to paid
