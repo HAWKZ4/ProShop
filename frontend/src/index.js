0d8be91000017ea82970d05708c46c7b4da3f7c9
Web Ghost <108879264+HAWKZ4@users.noreply.github.com>
2024-03-17 21:07:02 +0300

    Feat(index): Add 'HelmetProvider' tag to provide a context provider for 'react-helmet-async' package to work Feat(Meta): Introduce 'Meta' component for handling meta data to improve SEO and set the title of -each page (optional)- Feat(ProductScreen): Add 'Meta' component to 'productScreen' component to set the title of page according to product name Refactor(ProductEditScreen): Enhance user expreience by adding loader when image is uploading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import {
 } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { HelmetProvider } from "react-helmet-async";
 import store from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -65,10 +66,12 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PayPalScriptProvider deferLoading={true}>
-        <RouterProvider router={router} />
-      </PayPalScriptProvider>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <PayPalScriptProvider deferLoading={true}>
+          <RouterProvider router={router} />
+        </PayPalScriptProvider>
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -8,6 +8,7 @@ import { toast } from "react-toastify";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Meta from "../components/Meta";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
@@ -59,6 +60,7 @@ const ProductScreen = () => {
         <Message variant="danger">{error?.data?.message || error.error}</Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={5}>
               <Image src={product.image} alt={product.name} fluid />
@@ -138,7 +140,7 @@ const ProductScreen = () => {
                     <ListGroup.Item key={review._id}>
                       <strong>{review.name}</strong>
                       <Rating value={review.rating} />
-                      <p>{review.createdAt.substring(0,10)}</p>
+                      <p>{review.createdAt.substring(0, 10)}</p>
                       <p>{review.comment}</p>
                     </ListGroup.Item>
                   ))}
